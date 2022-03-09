65c65ba98438dfa7badbeb24cbb60c910f33ac81
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2022-03-09 13:40:25 +0000

    [STREAM]: Implement STREAM_LoadMainVram.

--- kain2/src/VRAM.C
+++ kain2/src/VRAM.C
@@ -8,6 +8,7 @@
 #pragma warning(disable: 4101)
 #endif
 
+short M_TrackClutUpdate;
 struct _BlockVramEntry* openVramBlocks; // offset 0x800D482C
 struct _BlockVramEntry* usedVramBlocks; // offset 0x800D4828
 struct _BlockVramEntry vramBlockList[90]; // offset 0x800D3E2C
