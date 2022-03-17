5b94bb74b3b16ea9a04c27aa01eb54bbd3c39b77
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2022-03-17 15:46:50 +0000

    [Platform-Emscripten]: Fix thread usage.

--- kain2/Platform/Emscripten.cmake
+++ kain2/Platform/Emscripten.cmake
@@ -1,6 +1,6 @@
 if(EMSCRIPTEN)
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -s USE_SDL=2 --emrun --std=c++11 --preload-file ${ROOT_DIR}/Build_Emscripten/Assets@/ -s USE_WEBGL2=1 -s FULL_ES3=1 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -lopenal")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -s USE_SDL=2 --emrun --std=c++11 --preload-file ${ROOT_DIR}/Build_Emscripten/Assets@/ -s USE_WEBGL2=1 -s FULL_ES3=1 -s USE_PTHREADS=1 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -lopenal")
 set(SDL2_LIBRARIES "-s USE_SDL=2")
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-error=format-security -Wno-narrowing -Wno-nonportable-include-path")
