a731b2432f4d5d329fad7e201f249543ae7c7491
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2022-03-20 08:57:22 +0000

    [GAMEPAD]: Fix bug when pressing Circle.

--- kain2/src/GAMEPAD.C
+++ kain2/src/GAMEPAD.C
@@ -633,7 +633,7 @@ void PSXPAD_TranslateData(long *data, unsigned short padData, unsigned short las
 											 0x1000, 0x0010,
 											 0x4000, 0x0080,
 											 0x8000, 0x0020,
-											 0x2000, 0x0048,
+											 0x2000, 0x0040,
 											 0x0400, 0x0100,
 											 0x0100, 0x0400,
 											 0x0800, 0x0200,
