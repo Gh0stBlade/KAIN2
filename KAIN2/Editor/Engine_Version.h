f8335ada8ca61dfed656163ace08a3d3e8244916
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2022-10-17 13:44:14 +0100

    [STREAM]: Implement WARPGATE_IncrementIndex.

--- KAIN2/Editor/Engine_Version.h
+++ KAIN2/Editor/Engine_Version.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define RES_GIT_COMMIT_STRING "1.0.1528\0"
-#define GIT_COMMIT_COUNT (1528)
-const int g_GIT_COMMIT_COUNT = 1528;
+#define RES_GIT_COMMIT_STRING "1.0.1529\0"
+#define GIT_COMMIT_COUNT (1529)
+const int g_GIT_COMMIT_COUNT = 1529;
