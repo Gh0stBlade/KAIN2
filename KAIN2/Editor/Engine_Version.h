9a83c320898833bb433cb3cccf94982ef8a9dabe
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2022-10-17 13:19:00 +0100

    [STREAM]: Implement STREAM_DumpAllUnitsNotNeeded.

--- KAIN2/Editor/Engine_Version.h
+++ KAIN2/Editor/Engine_Version.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define RES_GIT_COMMIT_STRING "1.0.1527\0"
-#define GIT_COMMIT_COUNT (1527)
-const int g_GIT_COMMIT_COUNT = 1527;
+#define RES_GIT_COMMIT_STRING "1.0.1528\0"
+#define GIT_COMMIT_COUNT (1528)
+const int g_GIT_COMMIT_COUNT = 1528;
