29d787b97f0c8f4781d95304a5ef861aedce98ae
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2022-10-17 13:12:37 +0100

    [BSP]: Implement SBSP_IntroduceInstances.

--- KAIN2/Editor/Engine_Version.h
+++ KAIN2/Editor/Engine_Version.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define RES_GIT_COMMIT_STRING "1.0.1526\0"
-#define GIT_COMMIT_COUNT (1526)
-const int g_GIT_COMMIT_COUNT = 1526;
+#define RES_GIT_COMMIT_STRING "1.0.1527\0"
+#define GIT_COMMIT_COUNT (1527)
+const int g_GIT_COMMIT_COUNT = 1527;
