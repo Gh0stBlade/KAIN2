4374a42a66ffcf63f8600e3002992a9cd53d3836
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2022-11-03 21:02:34 +0000

    [Valkyrie]: Remove NVN/GXM version.

--- Emulator/Platform/NX/Generate-Project-Files-Arm32.bat
+++
@@ -1,3 +0,0 @@
-cd ../../../
-cmake . -B"%cd%/Build/Build_NX_Arm/" -DCMAKE_BUILD_TYPE=NX_Release -DPLATFORM_NX_ARM=1 -G "Visual Studio 14 2015" -A Win32
-cd Build/Build_NX_Arm
--- Emulator/Platform/NX/Generate-Project-Files-Win32.bat
+++
@@ -1,3 +0,0 @@
-cd ../../../
-cmake . -B"%cd%/Build/Build_NX_Win/" -DCMAKE_BUILD_TYPE=Release -DPLATFORM_NX=1 -G "Visual Studio 17 2022" -A Win32
-cd Build/Build_NX_Win
