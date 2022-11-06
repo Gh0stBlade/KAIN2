7ffb8cb7dde5ba87cd162a13cb55a5d9e05a48c6
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2022-11-06 20:57:25 +0000

    [Editor]: Update.

--- KAIN2/Editor/Engine_Version.h
+++ KAIN2/Editor/Engine_Version.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define RES_GIT_COMMIT_STRING "1.0.2023\0"
-#define GIT_COMMIT_COUNT (2023)
-const int g_GIT_COMMIT_COUNT = 2023;
+#define RES_GIT_COMMIT_STRING "1.0.2024\0"
+#define GIT_COMMIT_COUNT (2024)
+const int g_GIT_COMMIT_COUNT = 2024;
