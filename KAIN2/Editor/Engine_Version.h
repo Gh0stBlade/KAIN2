121b559e2f96cbc452d83fc3745a7a24e83cd361
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2022-11-06 20:19:07 +0000

    [Editor]: Fix crash upon exit.

--- KAIN2/Editor/Engine_Version.h
+++ KAIN2/Editor/Engine_Version.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define RES_GIT_COMMIT_STRING "1.0.2015\0"
-#define GIT_COMMIT_COUNT (2015)
-const int g_GIT_COMMIT_COUNT = 2015;
+#define RES_GIT_COMMIT_STRING "1.0.2017\0"
+#define GIT_COMMIT_COUNT (2017)
+const int g_GIT_COMMIT_COUNT = 2017;
