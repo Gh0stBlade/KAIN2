a97754325da8b05ff57b92ed4c886fc38f0141c5
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2022-11-06 20:54:09 +0000

    [Valkyrie]: Make bounty list C.

--- KAIN2/Editor/Engine_Version.h
+++ KAIN2/Editor/Engine_Version.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define RES_GIT_COMMIT_STRING "1.0.2019\0"
-#define GIT_COMMIT_COUNT (2019)
-const int g_GIT_COMMIT_COUNT = 2019;
+#define RES_GIT_COMMIT_STRING "1.0.2023\0"
+#define GIT_COMMIT_COUNT (2023)
+const int g_GIT_COMMIT_COUNT = 2023;
