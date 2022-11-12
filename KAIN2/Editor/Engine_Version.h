f41744ca53d35bd61bde0c713ef62a3d9e6eacc6
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2022-11-12 16:16:31 +0000

    [Editor]: Fix LIBPNG warning.

--- KAIN2/Editor/Engine_Version.h
+++ KAIN2/Editor/Engine_Version.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define RES_GIT_COMMIT_STRING "1.0.2030\0"
-#define GIT_COMMIT_COUNT (2030)
-const int g_GIT_COMMIT_COUNT = 2030;
+#define RES_GIT_COMMIT_STRING "1.0.2070\0"
+#define GIT_COMMIT_COUNT (2070)
+const int g_GIT_COMMIT_COUNT = 2070;
