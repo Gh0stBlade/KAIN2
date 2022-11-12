cee4c74a039a798016e07df4245157f0c9a0e826
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2022-11-12 11:00:02 +0000

    [Valkyrie]: Fix Emscripten build.

--- Emulator/Platform/Emscripten/Generate-Project-Files.bat
+++ Emulator/Platform/Emscripten/Generate-Project-Files.bat
@@ -1,3 +1,4 @@
-emcmake cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_SHARED_LINKER_FLAGS="-sEXTRA_EXPORTED_RUNTIME_METHODS=Emulator_SetFocus"  -DCMAKE_EXE_LINKER_FLAGS="-sEXPORTED_RUNTIME_METHODS=ccall,cwrap -sEXTRA_EXPORTED_RUNTIME_METHODS=_GAMELOOP_RequestLevelChange" . -B%cd%"/Build_EMSDK/"
-cd ../../../Build_EMSDK
-cmake --build .
+cd ../../../
+emcmake cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_SHARED_LINKER_FLAGS="-sEXTRA_EXPORTED_RUNTIME_METHODS=Emulator_SetFocus"  -DCMAKE_EXE_LINKER_FLAGS="-sEXPORTED_RUNTIME_METHODS=ccall,cwrap -sEXTRA_EXPORTED_RUNTIME_METHODS=_GAMELOOP_RequestLevelChange" . -B%cd%"/Build/Build_EMSDK/"
+cd Build/Build_EMSDK/
+emcmake cmake --build .
