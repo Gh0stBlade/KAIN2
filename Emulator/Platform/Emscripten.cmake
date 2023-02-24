de5820a562b323e7d23e00bfe37a9d4389c9a4b6
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2023-02-24 15:48:09 +0000

    [Valkyrie]: Update Emscripten build system.

--- Emulator/Platform/Emscripten.cmake
+++ Emulator/Platform/Emscripten.cmake
@@ -5,9 +5,9 @@ set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 find_package(OpenGL REQUIRED)
 include_directories(${OPENGL_INCLUDE_DIR})
 
-set(CMAKE_SHARED_LINKER_FLAGS  "${CMAKE_SHARED_LINKER_FLAGS} -g3 -s TOTAL_MEMORY=1024MB -s USE_SDL=2 --std=c++11 -sASYNCIFY -s USE_WEBGL2=1 -s FETCH_DEBUG -s FULL_ES3=1 -s WASM=1 -s GLOBAL_BASE=0 -s ALLOW_MEMORY_GROWTH=1 -s USE_PTHREADS=1 -lopenal")
+set(CMAKE_SHARED_LINKER_FLAGS  "${CMAKE_SHARED_LINKER_FLAGS} -sASYNCIFY -s USE_SDL=2 --std=c++11 -s USE_WEBGL2=1 -s FULL_ES3=1 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s USE_PTHREADS=1")
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g3 -O2 -s USE_SDL=2 -s TOTAL_MEMORY=1024MB -Wno-error=format-security -Wno-narrowing -Wno-nonportable-include-path -pthread")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g3 -O2 -s USE_SDL=2 -s TOTAL_MEMORY=1024MB -Wno-error=format-security -Wno-narrowing -Wno-nonportable-include-path -pthread")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -s USE_SDL=2 -Wno-error=format-security -Wno-narrowing -Wno-nonportable-include-path -pthread")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O3 -s USE_SDL=2 -Wno-error=format-security -Wno-narrowing -Wno-nonportable-include-path -pthread")
 
 endif()
--- KAIN2/Game/Platform/Emscripten.cmake
+++ KAIN2/Game/Platform/Emscripten.cmake
@@ -1,15 +1,15 @@
 if(EMSCRIPTEN)
 
-set(CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} -sASYNCIFY --std=c++11 -s FETCH_DEBUG -s USE_WEBGL2=1 -s FULL_ES3=1 -s USE_PTHREADS=1 -s WASM=1 -s GLOBAL_BASE=0 -s ALLOW_MEMORY_GROWTH=1 -lopenal -s USE_SDL=2 -s TOTAL_MEMORY=512MB")
+set(CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} -sASYNCIFY --std=c++11 -s USE_WEBGL2=1 -s FULL_ES3=1 -s USE_PTHREADS=1 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s USE_SDL=2")
 
 set(SDL2_LIBRARIES "-s USE_SDL=2")
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g3 -O2 -s TOTAL_MEMORY=1024MB -Wno-error=format-security -Wno-narrowing -Wno-nonportable-include-path")
-set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -g3 -O2 -s TOTAL_MEMORY=1024MB -Wno-error=format-security -Wno-narrowing -Wno-nonportable-include-path")
-set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -g3 -O2 -s TOTAL_MEMORY=1024MB -Wno-error=format-security -Wno-narrowing -Wno-nonportable-include-path")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g3 -O2 -s TOTAL_MEMORY=1024MB -Wno-error=format-security -Wno-narrowing -Wno-nonportable-include-path")
-set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -g3 -O2 -s TOTAL_MEMORY=1024MB -Wno-error=format-security -Wno-narrowing -Wno-nonportable-include-path")
-set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -g3 -O2 -s TOTAL_MEMORY=1024MB -Wno-error=format-security -Wno-narrowing -Wno-nonportable-include-path")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O3  -Wno-error=format-security -Wno-narrowing -Wno-nonportable-include-path")
+set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -O3  -Wno-error=format-security -Wno-narrowing -Wno-nonportable-include-path")
+set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O3  -Wno-error=format-security -Wno-narrowing -Wno-nonportable-include-path")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3  -Wno-error=format-security -Wno-narrowing -Wno-nonportable-include-path")
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O3  -Wno-error=format-security -Wno-narrowing -Wno-nonportable-include-path")
+set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3  -Wno-error=format-security -Wno-narrowing -Wno-nonportable-include-path")
 
 link_directories(${ROOT_DIR}/Build_EMSDK/Emulator/)
 
