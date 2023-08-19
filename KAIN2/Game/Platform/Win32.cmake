dca4c7aafeac49bea6c8ab72dbe6984ff9c42585
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2023-08-19 19:15:27 +0100

    [Project]: Fix build.

--- KAIN2/Game/Platform/Win32.cmake
+++ KAIN2/Game/Platform/Win32.cmake
@@ -13,7 +13,8 @@ ${Vulkan_LIBRARY}
 )
 endif()
 
-target_link_directories(${PROJECT_NAME} PUBLIC ${ROOT_DIR}/Build/External/libValkyrie/$(Configuration))
+target_link_directories(${PROJECT_NAME} PUBLIC ${ROOT_DIR}/Build/Build_Win32/External/libValkyrie/$(Configuration))
+
 set(PSX_LIB Valkyrie.lib)
 
 endif()
