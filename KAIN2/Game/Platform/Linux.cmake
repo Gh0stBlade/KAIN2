8fe55b9148adde8beee814715ca059625e243883
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2023-09-09 14:05:09 +0100

    [Platform]: Linux fixes.

--- KAIN2/Game/Platform/Linux.cmake
+++ KAIN2/Game/Platform/Linux.cmake
@@ -7,6 +7,8 @@ set(PSX_LIB libValkyrie.a)
 
 target_link_directories(${PROJECT_NAME} PUBLIC ${ROOT_DIR}/Build_Linux/External/libValkyrie/$(Configuration))
 
+set(CMAKE_CXX_STANDARD 11)
+
 set(CMAKE_C_FLAGS           "${CMAKE_C_FLAGS} -fpermissive -Wno-narrowing")
 set(CMAKE_C_FLAGS_DEBUG     "${CMAKE_C_FLAGS_DEBUG} -fpermissive -Wno-narrowing")
 set(CMAKE_C_FLAGS_RELEASE   "${CMAKE_C_FLAGS_RELEASE} -fpermissive -Wno-narrowing1")
