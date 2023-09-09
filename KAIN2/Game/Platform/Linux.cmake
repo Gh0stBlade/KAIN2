a0589fd55fc584ea6745ea08c29c4d580b667e92
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2023-09-09 12:49:28 +0100

    [External]: Update libValkyrie.

--- KAIN2/Game/Platform/Linux.cmake
+++ KAIN2/Game/Platform/Linux.cmake
@@ -1,18 +1,7 @@
 if(UNIX AND NOT(ANDROID) AND NOT(EMSCRIPTEN) AND NOT (PLATFORM_NX_ARM) AND NOT (APPLE))
 
 #Find libs
-find_package(SDL2 REQUIRED)
 find_package(OpenGL REQUIRED)
-
-#We want to link GLEW statically
-set(GLEW_USE_STATIC_LIBS ON)
-find_package(GLEW REQUIRED)
-
-#We want to link GLEW statically
-set(GLEW_USE_STATIC_LIBS ON)
-
-#Setup project include directories
-include_directories(${SDL2_INCLUDE_DIR})
 
 set(PSX_LIB libValkyrie.a)
 
