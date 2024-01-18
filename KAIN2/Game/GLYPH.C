20c115d9c9b282988b077951ec14e964639405b2
Federico <federicoamilesi@gmail.com>
2024-01-18 09:53:56 -0300

    [GLYPH]: Update HUD_Init
    Matching - 100%
    https://decomp.me/scratch/i5nqv

--- KAIN2/Game/GLYPH.C
+++ KAIN2/Game/GLYPH.C
@@ -899,7 +899,7 @@ void HUD_Damp(short* val, short target, short* vel, short max)
 	CriticalDampValue(1, val, target, vel, &accl, max);
 }
 
-void HUD_Init() //Matching - 96.07%
+void HUD_Init()  // Matching - 100%
 { 
 	HUD_Rotation = 0;
 	HUD_Rot_vel = 0;
