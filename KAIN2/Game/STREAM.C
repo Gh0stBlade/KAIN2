6057f3dd114685a149e63ab596eabf6eefc3c9f2
Federico <fmilesi_soft@hotmail.com>
2024-01-30 21:18:48 -0300

    [STREAM]: Update MORPH_Continue
    Matching - 100%
    https://decomp.me/scratch/bscA7

--- KAIN2/Game/STREAM.C
+++ KAIN2/Game/STREAM.C
@@ -3345,7 +3345,7 @@ void MORPH_UpdateTextures()//Matching - 95%
 	MORPH_ChangeAreaPalettes(time);
 }
 
-void MORPH_Continue()
+void MORPH_Continue()  // Matching - 100%
 {
 	int i;
 	long mode;
@@ -3373,14 +3373,14 @@ void MORPH_Continue()
 
 	if (gameTrackerX.playerInstance->tface != NULL && mode >= 0)
 	{
-		if (gameTrackerX.playerInstance->tface == MORPH_SavedFace)
+		if (MORPH_SavedFace == gameTrackerX.playerInstance->tface)
 		{
 			MORPH_UpdateTrackingPoint(MORPH_SavedFace, MORPH_SavedLevel);
 		}
 		else
 		{
 			MORPH_GetComponentsForTrackingPoint(gameTrackerX.playerInstance->tface, (struct Level*)gameTrackerX.playerInstance->tfaceLevel);
-	
+
 			MORPH_UpdateTrackingPoint(gameTrackerX.playerInstance->tface, (struct Level*)gameTrackerX.playerInstance->tfaceLevel);
 		}
 	}
