26e1f44ddce80e141ffc220ee92ca610d9e1142b
Federico <fmilesi_soft@hotmail.com>
2024-02-13 00:33:23 -0300

    [STATE]: Update G2EmulationInstanceInitSection
    Matching - 100%
    https://decomp.me/scratch/hz19b

--- KAIN2/Game/STATE.C
+++ KAIN2/Game/STATE.C
@@ -1049,7 +1049,7 @@ void G2EmulationSetTotalSections(struct __CharacterState* In, short Total)
 	G2EmulationInstanceSetTotalSections(In->CharacterInstance, In->TotalSections);
 }
 
-void G2EmulationInstanceInitSection(struct _Instance* instance, int CurrentSection, long (*callback)(struct _G2Anim_Type* anim, int sectionID, enum _G2AnimCallbackMsg_Enum message, long messageDataA, long messageDataB, void* data), void* data)
+void G2EmulationInstanceInitSection(struct _Instance* instance, int CurrentSection, long (*callback)(struct _G2Anim_Type* anim, int sectionID, enum _G2AnimCallbackMsg_Enum message, long messageDataA, long messageDataB, void* data), void* data)  // Matching - 100%
 {
 	struct _G2AnimSection_Type* animSection;
 
